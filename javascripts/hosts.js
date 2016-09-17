8aceba6b876831bc97e19171a80dc3ca66e43251
yxf <yxf4559@163.com>
2016-09-17 09:45:46 +0800

    Update hosts.js

--- javascripts/hosts.js
+++ javascripts/hosts.js
@@ -26,3 +26,33 @@ function isValidHost(url){
   if( hosts.indexOf(url.hostname) >= 0 ) return true;
   return false;
 }
+function displaySearch(){
+  var html = '<div id="search"> \
+      <form action="https://movie.douban.com/subject_search" method="get" target="_blank" class="search__form"> \
+        <input name="search_text" type="search" placeholder="电影名" autocomplete="off" /> \
+        <input type="submit" value="豆瓣搜索" class="btn"> \
+      </form> \
+    </div>';
+  document.body.innerHTML = html;
+}
+
+function displayScore(params){
+  document.body.innerHTML = '<img src="spinner.gif" class="loading" id="loading">';
+  var url =  baseUrl + "?"
+  if(params) {
+    url += params.join("&");
+  }
+  
+  var iframe = document.createElement('iframe');
+  iframe.src = url;
+  iframe.style = "padding:0px; margin:0px; overflow:hidden;";
+  iframe.width = "100%";
+  iframe.height = "100%";
+  iframe.frameBorder = "0";
+  iframe.scrolling = "no";
+  iframe.onload = function(){
+    var loadingImg = document.getElementById("loading");
+    document.body.removeChild(loadingImg);
+  };
+  document.body.appendChild(iframe);
+}
